f5de397048eee81456cb56e54a3de45b742d154d
Jed <bibatjed@gmail.com>
2021-02-20 19:26:55 +0800

    feat: refactor auth controller

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -15,21 +15,25 @@ module.exports.loginUser = async (req, res, next) => {
             };
         }
 
-        if (userResult.validatePassword(password)) {
-            
-            //TODO: ADD PRIVATE KEY
-            const token = jwt.sign(
-                { id: userResult.id, username: userResult.username },
-                'shhhhh'
-            );
-
-            return res.json({
-                message: 'success',
-                result: {
-                    token,
-                },
-            });
+        if (!userResult.validatePassword(password)) {
+            throw {
+                status: 400,
+                message: 'Invalid Username or Password',
+            };
         }
+
+        //TODO: ADD PRIVATE KEY
+        const token = jwt.sign(
+            { id: userResult.id, username: userResult.username },
+            'shhhhh'
+        );
+
+        return res.json({
+            message: 'success',
+            result: {
+                token,
+            },
+        });
     } catch (e) {
         return next(e);
     }
